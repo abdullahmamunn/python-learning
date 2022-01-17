cd84ef34ea68a45cec3bbbebe61cdd788a473c46
Abdullah Al Mamun <almamun00021@gmail.com>
2022-01-17 12:20:43 +0600

    changes

--- max-min.py
+++ max-min.py
@@ -1,27 +1,30 @@
 # # Find maximum number in a list
-# numbers = [int(x) for x in input().split()]
-#
-# # Sort a list
-# numbers.sort()
-# min_sum = 0;
-# for item in numbers[0:4]:
-#     min_sum += item
-# max_sum = 0
-# for item in numbers[1:5]:
-#     max_sum += item
-# print(min_sum,max_sum)
+numbers = [int(x) for x in input().split()]
+
+# Sort a list
+numbers.sort()
+min_sum = 0;
+for item in numbers[0:4]:
+    min_sum += item
+max_sum = 0
+for item in numbers[1:5]:
+    max_sum += item
+print(min_sum,max_sum)
 
 
 # Find maximum number in a list
-# numbers = [int(x) for x in input().split()]
-#
-# # Sort a list
-# numbers.sort()
-# min_sum = sum(numbers[0:4])
-# max_sum = sum(numbers[1:5])
-# print(min_sum,max_sum)
+numbers = [int(x) for x in input().split()]
+
+# Sort a list
+numbers.sort()
+min_sum = sum(numbers[0:4])
+max_sum = sum(numbers[1:5])
+print(min_sum,max_sum)
+
+
 
 # Transfer to function
+
 def minimaxsum(num):
     num.sort()
     min_sum = sum(num[0:4])
